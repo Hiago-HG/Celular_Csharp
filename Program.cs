ecd1635a27485a838047ad29e69b0e5c36688702
Hiago-HG <90069490+Hiago-HG@users.noreply.github.com>
2024-11-16 16:37:35 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+Console.Write("\n");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(numero: "8674-4521", modelo: "2020", imei: "1ph0n3", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.Write("\n");
+
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Iphone(numero: "3233-4584", modelo: "2021", imei: "n0k14", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Nubank");
+
+Console.Write("\n");
+
+Console.WriteLine($"Numero do Nokia : {nokia.Numero}");
+nokia.Numero = "9535-6800";
+Console.WriteLine($"Novo numero do Nokia : {nokia.Numero}");
+
